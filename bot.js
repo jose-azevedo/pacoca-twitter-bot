aa35319fee67be434d4191c0fb3aa74379abdb26
José Roberto Azevedo <66346043+joseazevedo47@users.noreply.github.com>
2020-08-14 23:15:39 -0300

    Tweets are built as they are called

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 import Twitter from 'twitter';
+import tweetLib from './tweetLibrary.json';
 import config from './config.js';
 import fs from 'fs'; 
 
@@ -8,7 +9,6 @@ console.log("The dog is alive");
 
 function makeStatusRandom(tweetChar) {
     const repetitions = Math.floor(Math.random()*15);
-    console.log(`Repetições: ${repetitions}`)
     var tweetStatus = '';
     for(var i = 0; i < repetitions; i++){
         if (Math.random() > 0.5) {
@@ -21,98 +21,117 @@ function makeStatusRandom(tweetChar) {
     return tweetStatus;
 }
 
-const timedTweets = [
-    {
-        action: 'sleep',
-        status: 'ZZzzzZZZz' + makeStatusRandom('z') + '.....',
-        picPath: 'fotos/dormindo.jpg'
-    },
-    {
-        action: 'bored',
-        status: 'Meh',
-        picPath: 'fotos/tedio.jpg'
-    },
-    {
-        action: 'pee',
-        status: '*xiii' + makeStatusRandom('i') + 'iii*',
-        picPath: 'fotos/xixi.jpg'
-    },
-    {
-        action: 'angry',
-        status: 'GRRR' + makeStatusRandom('r'),
-        picPath: 'fotos/raiva.jpg'
-    },
-    {
-        action: 'greet',
-        status: 'WOUF',
-        picPath: 'fotos/deboa.jpg'
-    },
-    {
-        action: 'swim',
-        status: 'WOSH' + makeStatusRandom('h'),
-        picPath: 'fotos/nadando.jpg'
-    },
-    {
-        action: 'ehmole',
-        status: 'ehmole kkkkkk' + makeStatusRandom('k'),
-        picPath: 'fotos/ehmole.jpg'
-    },
-    {
-        action: 'sad', 
-        status: '*cãin*',
-        picPath: 'fotos/triste.jpg'
-    },
-    {
-        action: 'poo',
-        status: makeStatusRandom('.'),
-        picPath: 'fotos/cagando.png'
-    },
-    {
-        action: '#amopacoca',
-        status: '#amopaçoca',
-        picPath: 'fotos/profile_pic.png'
-    },
-    {
-        action: 'dive',
-        status: '*TCHBU' + makeStatusRandom('u') + 'M*',
-        picPath: 'fotos/tchbum.jpg'
-    },
-    {
-        action: 'lick',
-        status: '*slip*',
-        picPath: 'fotos/lambida.jpg'
-    },
-    {
-        action: 'scratch',
-        status: 'unhe' + makeStatusRandom('e'),
-        picPath: 'fotos/coçada.jpg'
-    },
-    {
-        action: 'cold',
-        status: 'BBRR' + makeStatusRandom('r'),
-        picPath: 'fotos/neve.jpg'
-    },
-    {
-        action: 'highfive',
-        status: 'AU',
-        picPath: 'fotos/highfive.jpg'
-    },
-    {
-        action: 'scared',
-        status: makeStatusRandom('.'),
-        picPath: 'fotos/assustado.jpg'
-    },
-    {
-        action: 'love',
-        status: makeStatusRandom('WOUF'),
-        picPath: 'fotos/amopacoca.jpg'
-    },
-    {
-        action: 'bark',
-        status: makeStatusRandom('AU'),
-        picPath: ''
+function fetchTweet(requestedTweet) {
+    const tweetLibrary = [
+        {
+            action: 'sleep',
+            status: 'ZZzzzZZZz' + makeStatusRandom('z') + '.....',
+            picPath: 'fotos/dormindo.jpg'
+        },
+        {
+            action: 'bored',
+            status: 'Meh',
+            picPath: 'fotos/tedio.jpg'
+        },
+        {
+            action: 'pee',
+            status: '*xiii' + makeStatusRandom('i') + 'iii*',
+            picPath: 'fotos/xixi.jpg'
+        },
+        {
+            action: 'angry',
+            status: 'GRRR' + makeStatusRandom('r'),
+            picPath: 'fotos/raiva.jpg'
+        },
+        {
+            action: 'greet',
+            status: 'WOUF',
+            picPath: 'fotos/deboa.jpg'
+        },
+        {
+            action: 'swim',
+            status: 'WOSH' + makeStatusRandom('h'),
+            picPath: 'fotos/nadando.jpg'
+        },
+        {
+            action: 'ehmole',
+            status: 'ehmole kkkkkk' + makeStatusRandom('k'),
+            picPath: 'fotos/ehmole.jpg'
+        },
+        {
+            action: 'sad', 
+            status: '*cãin*',
+            picPath: 'fotos/triste.jpg'
+        },
+        {
+            action: 'poo',
+            status: makeStatusRandom('.'),
+            picPath: 'fotos/cagando.png'
+        },
+        {
+            action: '#amopacoca',
+            status: '#amopaçoca',
+            picPath: 'fotos/profile_pic.png'
+        },
+        {
+            action: 'dive',
+            status: '*TCHBU' + makeStatusRandom('u') + 'M*',
+            picPath: 'fotos/tchbum.jpg'
+        },
+        {
+            action: 'lick',
+            status: '*slip*',
+            picPath: 'fotos/lambida.jpg'
+        },
+        {
+            action: 'scratch',
+            status: 'unhe' + makeStatusRandom('e'),
+            picPath: 'fotos/coçada.jpg'
+        },
+        {
+            action: 'cold',
+            status: 'BBRR' + makeStatusRandom('r'),
+            picPath: 'fotos/neve.jpg'
+        },
+        {
+            action: 'highfive',
+            status: 'AU',
+            picPath: 'fotos/highfive.jpg'
+        },
+        {
+            action: 'scared',
+            status: makeStatusRandom('.'),
+            picPath: 'fotos/assustado.jpg'
+        },
+        {
+            action: 'love',
+            status: makeStatusRandom('WOUF'),
+            picPath: 'fotos/amopacoca.jpg'
+        },
+        {
+            action: 'bark',
+            status: makeStatusRandom('AU'),
+            picPath: ''
+        }
+    ]
+
+    console.log(`Tweet requisitado: ${requestedTweet}`)
+    if (typeof(requestedTweet) === 'number') {
+        return tweetLibrary[requestedTweet];
     }
-]
+
+    if (typeof(requestedTweet) === 'string') {
+        
+        console.log(`Parâmetro é uma string`)
+        var chosenTweet;
+        tweetLibrary.map((tweet) => {
+            if (tweet.action == requestedTweet) {
+                chosenTweet = tweet;
+            }
+        });
+        return chosenTweet
+    }
+}
 
 function regularBark(action) {
     var pic = fs.readFileSync(action.picPath);
@@ -140,7 +159,7 @@ function regularBark(action) {
 function chooseAction() {
     const actionId = Math.floor(Math.random()*14);
     
-    const chosenAction = timedTweets[actionId];
+    const chosenAction = fetchTweet(actionId);
 
     console.log(`Ação escolhida: ${actionId}`);
 
@@ -167,7 +186,7 @@ stream.on('error', function(error) {
 
 function getTweet(action) {
     var chosenTweet;
-    timedTweets.map((tweet, index) => {
+    tweetLibrary.map((tweet, index) => {
         if (tweet.action == action) {
             chosenTweet = tweet;
         }
@@ -179,27 +198,27 @@ function decideReaction(tweetText) {
     if(tweetText.search('amo') > -1 || tweetText.search('adoro') > -1 || tweetText.search('eu gosto de paçoca') > -1 || tweetText.search('gostoso') > -1 || tweetText.search('delícia') > -1 || tweetText.search('bom') > -1){
         return {
             foundReaction: true,
-            replyTweet: getTweet('love')
+            replyTweet: fetchTweet('love')
         };
     } else if(tweetText.search('odeio') > -1 || tweetText.search('não gosto') > -1 || tweetText.search('horrível') > -1 || tweetText.search('ruim') > -1){
         return {
             foundReaction: true,
-            replyTweet: getTweet('angry')
+            replyTweet: fetchTweet('angry')
         };
     } else if(tweetText.search('comer') > -1 || tweetText.search('comi') > -1 || tweetText.search('comendo') > -1){
         return {
             foundReaction: true,
-            replyTweet: getTweet('scared')
+            replyTweet: fetchTweet('scared')
         };
     } else if(tweetText.search('@pacoca_acaxorra') > -1){
         return {
             foundReaction: true,
-            replyTweet: getTweet('highfive')
+            replyTweet: fetchTweet('highfive')
         };
     } else {
         return {
             foundReaction: false,
-            replyTweet: getTweet('bark')
+            replyTweet: fetchTweet('bark')
         };
     }
 }
