9891fd56ce2facccfb863aefe1451c72eb7eeb4c
José Azevedo <jose@espressolabs.com.br>
2021-12-29 15:55:12 -0300

    Deleção de arquivos JavaScript defasados

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    consumer_key: '85nsbxf2Gk8KMOWzGUvOJO02I',
-    consumer_secret: 'qdrbGfIOlSAuR3TsB2oTN59VTn4JGZ9HvliLUfa3YzAMvCbzJk',
-    access_token_key: '1279974945041367041-LtXktDGUxs9xnro73XeBFPfh55mdJn',
-    access_token_secret: 'Kc3Qi3iXDecUQTKw052oFu6IMm3n5YwtOvf6fNKtWglvF'
-};
-
-export default config;
